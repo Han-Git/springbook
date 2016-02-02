8053f5a50383757b62bdc157c076c9695a002108
Han <topofstar0@gmail.com>
2016-02-02 17:40:16 +0900

    study #2

--- springbook/src/springbook/user/dao/UserDao.java
+++ springbook/src/springbook/user/dao/UserDao.java
@@ -9,38 +9,11 @@ import java.sql.SQLException;
 import springbook.user.domain.User;
 
 public class UserDao {
-	public void add(User user) throws ClassNotFoundException, SQLException{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "jmh", "jmh");
-		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-		
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
-	}
+	private SimpleConnectionMaker simpleConnectionMaker;
 	
-	public User get(String id) throws ClassNotFoundException, SQLException{
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "jmh", "jmh");
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
-		
-		rs.close();
-		ps.close();
-		c.close();
-		return user;
+	public UserDao() {
+		simpleConnectionMaker = new SimpleConnectionMaker();
 	}
-	
 	public static void main(String[] args)throws ClassNotFoundException, SQLException {
 		UserDao dao = new UserDao();
 		
@@ -60,4 +33,35 @@ public class UserDao {
 		System.out.println(user2.getId() + "조회 성공!");
 		
 	}
+	
+	public void add(User user) throws ClassNotFoundException, SQLException{
+		Connection c = simpleConnectionMaker.makeNewConnection();
+		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException{
+		Connection c = simpleConnectionMaker.makeNewConnection();
+		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
+		ps.setString(1, id);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		return user;
+	}
+	
 }
