f61e33c025a50494e3c841e94677805bfb655194
Han <topofstar0@gmail.com>
2016-03-02 16:26:26 +0900

    spring study #p250
    #template/callback method test#2

--- springbook/src/springbook/learningtest/template/Calculator.java
+++ springbook/src/springbook/learningtest/template/Calculator.java
@@ -7,16 +7,27 @@ import java.io.IOException;
 public class Calculator {
 
 	public Integer calcSum(String filepath)throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(filepath));	//#p249
-		Integer sum = 0;	//#p249
-		String line = null;	//#p249
-		while((line = br.readLine()) != null){	//#p249
-			sum += Integer.valueOf(line);	//#p249
+		BufferedReader br = null;	// #p250
+		try{ 
+			br = new BufferedReader(new FileReader(filepath));	//#p250
+			Integer sum = 0;	//#p249
+			String line = null;	//#p249
+			while((line = br.readLine()) != null){	//#p249
+				sum += Integer.valueOf(line);	//#p249
+			}
+			
+			//br.close();	//#p249
+			
+			return sum;	//#p249
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+			throw e;
+		}finally {
+			if(br != null){
+				try{br.close();}	//#p250
+				catch(IOException e){System.out.println(e.getMessage());}
+			}
 		}
-		
-		br.close();	//#p249
-		
-		return sum;	//#p249
 	}
 	
 }
