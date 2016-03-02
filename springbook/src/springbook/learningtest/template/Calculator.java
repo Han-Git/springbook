3254f48c1d44a5d1b010ecb6e641d507b745d0af
Han <topofstar0@gmail.com>
2016-03-02 16:42:19 +0900

    spring study #p253
    #template/callback method test3

--- springbook/src/springbook/learningtest/template/Calculator.java
+++ springbook/src/springbook/learningtest/template/Calculator.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class Calculator {
 
 	public Integer calcSum(String filepath)throws IOException {
+		/*	//p253
 		BufferedReader br = null;	// #p250
 		try{ 
 			br = new BufferedReader(new FileReader(filepath));	//#p250
@@ -28,6 +29,39 @@ public class Calculator {
 				catch(IOException e){System.out.println(e.getMessage());}
 			}
 		}
+		*/
+		
+		BufferedReaderCallback sumCallback = 	//#p253
+			new BufferedReaderCallback(){	//#p253
+				public Integer doSomethingWithReader(BufferedReader br) throws IOException {	//#p253
+					Integer sum = 0;	//#p253
+					String line = null;	//#p253
+					while((line = br.readLine()) != null){	//#p253
+						sum += Integer.valueOf(line);	//#p253
+					}	//#p253
+					return sum;	//#p253
+				}	//#p253
+			};	//#p253
+		return fileReadTemplate(filepath, sumCallback);	//#p253
 	}
 	
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
+		BufferedReader br = null;
+		try{
+			br = new BufferedReader(new FileReader(filepath));
+			int ret = callback.doSomethingWithReader(br);
+			return ret;
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+			throw e;
+		}finally {
+			if(br != null){
+				try{
+					br.close();
+				}catch(IOException e){
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+	}
 }
