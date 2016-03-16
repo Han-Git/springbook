9f26c32d14f199dc2b90e3bb94dedaaff6750f8c
Han <topofstar0@gmail.com>
2016-03-16 16:51:34 +0900

    spring study Ch5. P319

--- springbookCh5/src/springbook/user/domain/User.java
+++ springbookCh5/src/springbook/user/domain/User.java
@@ -1,6 +1,11 @@
 package springbook.user.domain;
 
 public class User {
+	private static final int BASIC = 1;
+	private static final int SILVER = 2;
+	private static final int GOLD = 3;
+	
+	int level;
 	String id;
 	String name;
 	String password;
@@ -37,6 +42,14 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
 	}
 	
 }
