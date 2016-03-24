79d0d78c9567286116a04cca1056951d7bb97ead
Han <topofstar0@gmail.com>
2016-03-24 20:37:59 +0900

    spring study p412
    #AOP error2

--- springbookCh6/src/springbook/user/service/UserServiceImpl.java
+++ springbookCh6/src/springbook/user/service/UserServiceImpl.java
@@ -9,7 +9,7 @@ import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
 	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
 	public static final int MIN_RECCOMEND_FOR_GOLD = 30;
 	
@@ -24,7 +24,7 @@ public class UserServiceImpl {
 		this.userDao = userDao;
 	}
 	
-	public void upgradeLevels() throws Exception{
+	public void upgradeLevels(){
 		try{
 			List<User> users = userDao.getAll();
 			for(User user : users){
